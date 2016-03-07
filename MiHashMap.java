f94f646649f37d006d1e40e11cf7ce4f533b6a06
KevinFN04 <kevin.felix.04@gmail.com>
2016-03-07 12:25:11 +0100

    Commit inicial.

---
+++ MiHashMap.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    /**
+     * Constructor de la clase MiHashMap
+     */
+    public MiHashMap(){
+        
+    }
+}
